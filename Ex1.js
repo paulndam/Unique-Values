d649acf107555918e6a7ac8678cf310c34195e10
Paul Ndam <paulndam@Pauls-MacBook-Air-2.local>
2021-01-25 22:24:45 -0500

    two

--- Ex1.js
+++ Ex1.js
@@ -30,29 +30,29 @@ console.log(sumZero([-4, -3, -2, -1, 0, 5, 8, 7])); // out put is undefined
 // function that accepts a sorted array and counts the unique values in the array, negative numbers can be included in the array.
 
 function countUniqueValues(array) {
-  
-    if(array.length === 0){
-        return `There's no number in the array`
-    }
+  if (array.length === 0) {
+    return `There's no number in the array`;
+  }
 
   let i = 0;
 
   for (let j = 1; j < array.length; j++) {
-    if (array[i] === array[j]) {
-      array[j]++;
-    } else if (array[i] !== array[j]) {
-      array[i]++;
+    if (array[i] !== array[j]) {
+      i ++;
       array[i] = array[j];
+      console.log(`---- array index i is --> ${array[i]}-----`)
+      console.log(`---- i is --> ${array[i]}-----`)
     }
 
     //console.log(i, j);
   }
 
-  return array[i];
+  return i + 1;
 }
 
 console.log(countUniqueValues([1, 1, 1, 1, 1, 2])); // out put is 2 unique numbers
 console.log(countUniqueValues([1, 2, 3, 4, 4, 5, 5, 6])); // out put is 6 unique numbers
 console.log(countUniqueValues([])); // out put is undefined
-console.log(countUniqueValues([-3, -2, -1, -1, 2, 2])); // out put is 2 unique numbers
-console.log(countUniqueValues([1,1,1,2,2,3,4,5,5,5,6,7])); // out put is 7 unique numbers
+console.log(countUniqueValues([-3, -2, -1, -1, 2, 2])); // out put is 4 unique numbers
+console.log(countUniqueValues([1, 1, 1, 2, 2, 3, 4, 5, 5, 5, 6, 7])); // out put is 7 unique numbers
+console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]));// out put is 7 unique numbers
